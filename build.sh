a580a57c73f2c8240eb05c14053ece02a8b68ce4
marco <marcaux@cryptolab.net>
2021-11-26 11:43:19 +0100

    experiement why mobile editor styles aren't working

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+rm sn-vscode-theme.zip
+zip -r sn-vscode-theme.zip . -x build -x .\* -x \*.zip -x node_modules/\*
+
+if [ $1 = "debug" ]; then
+  unameOut="$(uname -s)"
+  case "${unameOut}" in
+      Linux*)     machine=Linux;;
+      Darwin*)    machine=Mac;;
+      CYGWIN*)    machine=Cygwin;;
+      MINGW*)     machine=MinGw;;
+      *)          machine="UNKNOWN:${unameOut}"
+  esac
+
+  if [ $machine == "Linux" ];
+  then
+    if [ -d "$HOME/.config/Standard Notes/Extensions/vscode-theme" ];
+    then
+      rm -fr "$HOME/.config/Standard Notes/Extensions/vscode-theme/"*
+    else
+      mkdir -p "$HOME/.config/Standard Notes/Extensions/vscode-theme"
+    fi
+  fi
+
+  unzip sn-vscode-theme.zip -d "$HOME/.config/Standard Notes/Extensions/vscode-theme"
+fi
+
+exit 0
